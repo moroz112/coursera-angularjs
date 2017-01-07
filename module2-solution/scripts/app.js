03c4d0b89bb2bc44316ab10cc23a2e535b370b9b
amoroz <morozalexrpd@gmail.com>
2017-01-07 17:20:09 +0200

    implementation solution for module2

--- module2-solution/scripts/app.js
+++ module2-solution/scripts/app.js
@@ -1,16 +1,33 @@
 (function() {
     angular.module('ShoppingListCheckOff', [])
         .controller('ToBuyController', ToBuyController)
-        .controller('AlreadyBoughtController', AlreadyBoughtController).
-        service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+        .controller('AlreadyBoughtController', AlreadyBoughtController)
+        .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-    function ToBuyController() {
+    ToBuyController.$inject = ['ShoppingListCheckOffService'];
+    AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
 
+    function ToBuyController(ShoppingListCheckOffService) {
+        this.toBuyItems = ShoppingListCheckOffService.toBuyItems;
+
+        this.boughtItem = function(index, name, quantity) {
+            ShoppingListCheckOffService.boughtItem(index, name, quantity)
+        }
     }
-    function AlreadyBoughtController() {
-
+    function AlreadyBoughtController(ShoppingListCheckOffService) {
+        this.boughtItems = ShoppingListCheckOffService.boughtItems;
     }
     function ShoppingListCheckOffService() {
+        this.toBuyItems = [
+            {name: 'cookies', quantity: 10},
+            {name: 'cakes', quantity: 5},
+            {name: 'candies', quantity: 11}
+        ];
+        this.boughtItems = [];
 
+        this.boughtItem = function(index, name, quantity) {
+            this.toBuyItems.splice(index, 1);
+            this.boughtItems.push({name: name, quantity: quantity});
+        }
     }
 })();
