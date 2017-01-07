cb559dab82d6cb4aef13d5edce6c32149019ddf8
amoroz <morozalexrpd@gmail.com>
2017-01-07 16:48:29 +0200

    start develop module2-solution

---
+++ module2-solution/scripts/app.js
@@ -0,0 +1,16 @@
+(function() {
+    angular.module('ShoppingListCheckOff', [])
+        .controller('ToBuyController', ToBuyController)
+        .controller('AlreadyBoughtController', AlreadyBoughtController).
+        service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+    function ToBuyController() {
+
+    }
+    function AlreadyBoughtController() {
+
+    }
+    function ShoppingListCheckOffService() {
+
+    }
+})();
